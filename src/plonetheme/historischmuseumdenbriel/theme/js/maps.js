8188714235cb55afc95a210cd8a4cb6725fd7625
Steven Hoogervorst <s.hoogervorst@hotmail.com>
2019-03-12 15:56:32 +0100

    Implemented timeout for loading map

--- src/plonetheme/historischmuseumdenbriel/theme/js/maps.js
+++ src/plonetheme/historischmuseumdenbriel/theme/js/maps.js
@@ -1,30 +1,39 @@
 
-function initMap() {
-	var lat = document.getElementById("googleMap").getAttribute("lat");
-	var lng = document.getElementById("googleMap").getAttribute("lng");
+var lat = document.getElementById("googleMap").getAttribute("lat").val();
+var lng = document.getElementById("googleMap").getAttribute("lng").val();
 
-	//GOOGLE MAPS ROUTE
-	var myCenter=new google.maps.LatLng(lat,lng);
-	var mapProp = {
-	  center:myCenter,
-	  zoom:15,
-	  disableDefaultUI:true, 
-							preserveViewport: true,
-							panControl:true,
-	    					zoomControl:true,
-	    					mapTypeControl:true,
-	    					scaleControl:false,
-							streetViewControl:false,
-							overviewMapControl:false,
-							rotateControl:false, 
-	  mapTypeId:google.maps.MapTypeId.ROADMAP
-	  };
+//GOOGLE MAPS ROUTE
+var myCenter=new google.maps.LatLng(lat,lng);
 
-	var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+setTimeout(startMap(), 1);
 
-	var marker=new google.maps.Marker({
-	  position:myCenter
-	  });
+function startMap() {
 
-	marker.setMap(map);
+	function initialize() {
+		var mapProp = {
+		  center:myCenter,
+		  zoom:15,
+		  disableDefaultUI:true, 
+								preserveViewport: true,
+								panControl:true,
+		    					zoomControl:true,
+		    					mapTypeControl:true,
+		    					scaleControl:false,
+								streetViewControl:false,
+								overviewMapControl:false,
+								rotateControl:false, 
+		  mapTypeId:google.maps.MapTypeId.ROADMAP
+		  };
+
+		var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+		var marker=new google.maps.Marker({
+		  position:myCenter
+		  });
+
+		marker.setMap(map);
+	}
+
+	google.maps.event.addDomListener(window, 'load', initialize);
+
 }
