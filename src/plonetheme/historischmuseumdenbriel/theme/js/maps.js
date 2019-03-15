2ba7025460b2aa9146729265771dc4ccbb568377
Steven Hoogervorst <s.hoogervorst@hotmail.com>
2019-03-15 12:27:49 +0100

    Only load Google Maps if Google map division is available on page

--- src/plonetheme/historischmuseumdenbriel/theme/js/maps.js
+++ src/plonetheme/historischmuseumdenbriel/theme/js/maps.js
@@ -8,32 +8,33 @@ var myCenter=new google.maps.LatLng(lat,lng);
 setTimeout(startMap(), 1);
 
 function startMap() {
+	if (document.getElementById("googleMap")) {
+		function initialize() {
+			var mapProp = {
+			  center:myCenter,
+			  zoom:15,
+			  disableDefaultUI:true, 
+									preserveViewport: true,
+									panControl:true,
+			    					zoomControl:true,
+			    					mapTypeControl:true,
+			    					scaleControl:false,
+									streetViewControl:false,
+									overviewMapControl:false,
+									rotateControl:false, 
+			  mapTypeId:google.maps.MapTypeId.ROADMAP
+			  };
 
-	function initialize() {
-		var mapProp = {
-		  center:myCenter,
-		  zoom:15,
-		  disableDefaultUI:true, 
-								preserveViewport: true,
-								panControl:true,
-		    					zoomControl:true,
-		    					mapTypeControl:true,
-		    					scaleControl:false,
-								streetViewControl:false,
-								overviewMapControl:false,
-								rotateControl:false, 
-		  mapTypeId:google.maps.MapTypeId.ROADMAP
-		  };
+			var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
 
-		var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+			var marker=new google.maps.Marker({
+			  position:myCenter
+			  });
 
-		var marker=new google.maps.Marker({
-		  position:myCenter
-		  });
+			marker.setMap(map);
+		}
 
-		marker.setMap(map);
+		google.maps.event.addDomListener(window, 'load', initialize);
 	}
-
-	google.maps.event.addDomListener(window, 'load', initialize);
 
 }
